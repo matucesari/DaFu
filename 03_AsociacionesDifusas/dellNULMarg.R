4b511c373314014891ae3bd79d79d6a4fea9a18e
Matilde Inés Césari <85371512+matucesari@users.noreply.github.com>
2024-05-31 10:21:10 -0300

    arreglos

--- 03_AsociacionesDifusas/dellNULMarg.R
+++
@@ -1,8 +0,0 @@
-# Filtrar columnas marginales nulas de una tabla de contigencia
-
-dellNULMarg <- function(tabla) {
-  # Reemplazar valores faltantes con cero en una tabla llamada 'mi_tabla'
-  tabla[is.na(tabla)] <- 0
-  suma_columnas <- colSums(tabla)
-    return(as.data.frame(tabla[, suma_columnas != 0]))
-}
